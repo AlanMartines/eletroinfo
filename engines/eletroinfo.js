9795662b16c23e1b9ab2bd95aa57aaf8fed03228
AlanMartines <alancpmartines@hotmail.com>
2024-11-17 18:33:17 -0400

    Calculadora de IP (IPv4)

--- engines/eletroinfo.js
+++ engines/eletroinfo.js
@@ -190,12 +190,11 @@ router.post('/CalculadoraIPv4', async (req, res, next) => {
 		});
 	} catch (error) {
 		// Capturando e retornando erro interno
-		console.log(error);
 		return res.status(500).json({
 			error: true,
 			status: 500,
 			result: null,
-			message: 'Erro ao calcular a sub-rede'
+			message: 'Erro durante o cálculo.'
 		});
 	}
 
