87676c836df209d4f92479ef5357721e96754dfc
Alan Martines <alancpmartines@hotmail.com>
2024-11-22 10:06:53 -0400

    Update

--- middleware/calculateIP.js
+++ middleware/calculateIP.js
@@ -55,6 +55,7 @@ function calculateIPInfo(ipAddress, subnetMask) {
 			usableIPRange: `${network.hostFirst()} - ${network.hostLast()}`,
 			totalHosts: network.networkSize().toString(),
 			cidrNotation: subnetMask,
+			ipType: ipType,
 			shortIp: `${network.address}${subnetMask}`
 		};
 	}
